e455a865b1620ffbc2e7bc9800d3d84a3d736f8f
sayeedfariha <sayeedfariha79@gmail.com>
2024-11-11 00:21:45 +0600

    17

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,23 +1,36 @@
+"use client";
 import React from "react";
 import Image from "next/image";
-import logo from "../assets/ecwid-logo.png";
+import logo from "../assets/logo.png";
 import Link from "next/link";
+import Hamburger from "hamburger-react";
 
 function Header() {
   return (
-    <header className="bg-white w-full h-16 sticky top-0 flex items-center gap-10 px-8 xl:px-20 z-[1000]">
+    <header className="bg-white w-full h-16 sticky top-0 flex items-center justify-between gap-10 px-8 xl:px-20 z-[1000]">
       <Link href="#" className="flex items-center">
-            <Image
-              src={logo}
-              alt="ecwid-Logo"
-              height={100}
-              width={100}
-              className="h-auto w-20"
-            />
-          </Link>
+        <Image
+          src={logo}
+          alt="ecwid-Logo"
+          height={100}
+          width={100}
+          className="h-auto w-20"
+        />
+      </Link>
+
+      <div className="flex xl:hidden">
+        <Hamburger
+          onToggle={(toggled) => {
+            if (toggled) {
+              ``
+            } else {
+              // close a menu
+            }
+          }}
+        />
+      </div>
       <div className="w-full hidden xl:flex items-center justify-between">
         <div className="logo flex items-center gap-10">
-
           <ul className="flex gap-10">
             <li>Sell</li>
             <li>Simplify</li>
